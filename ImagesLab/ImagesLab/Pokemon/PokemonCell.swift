236af48f0ecd49344e33c22086210b6e9ab9eb15
Casandra <casandragrullon@pursuit.org>
2019-12-09 14:25:23 -0500

    finished pokemon part of app

--- ImagesLab/ImagesLab/Pokemon/PokemonCell.swift
+++ ImagesLab/ImagesLab/Pokemon/PokemonCell.swift
@@ -11,10 +11,12 @@ import UIKit
 class PokemonCell: UITableViewCell {
 
     @IBOutlet weak var pokemonThumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
     
     var pokemon: Cards?
     
     func configureCell(for poke: Cards){
+        nameLabel.text = poke.name
         NetworkHelper.shared.performDataTask(with: poke.imageUrl) { [unowned self] (result) in
             switch result{
             case .failure(let appError):
